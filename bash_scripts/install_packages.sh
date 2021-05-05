3d37416dec7fa5ee02bd6a1b7feba8db51c64a08
suusardzh <suusardzh@gmail.com>
2021-05-05 12:14:03 +0000

    modified bash_scripts/install_packages.sh

--- bash_scripts/install_packages.sh
+++ bash_scripts/install_packages.sh
@@ -5,7 +5,7 @@ packages="vim wget curl git epel-release"
 dependent_packages="htop"
 
   sudo yum install $packages -y ; 
-  if [$? -eq 0]; then
+  if [$? -eq 0 ]; then
    sudo yum install $dependent_packages -y
 fi
  echo "Succesfully Installed $packages $dependent_packages."
